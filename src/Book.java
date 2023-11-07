3a008a0ab8381f0f8a2b1126ccfeb791ba858509
MaximovMaxim <maximovmaxim100500@gmail.com>
2023-11-08 00:01:48 +0300

    домашка готова

---
+++ src/Book.java
@@ -0,0 +1,23 @@
+public class Book {
+    private String bookTitle;
+    private Author author;
+    private int yearPublication;
+    public Book(String bookTitle, Author author, int yearPublication) {
+        this.bookTitle = bookTitle;
+        this.author = author;
+        this.yearPublication = yearPublication;
+    }
+    public String getBookTitle() {
+        return this.bookTitle;
+    }
+    public Author getAuthor() {
+        return this.author;
+    }
+    public int getYearPublication() {
+        return this.yearPublication;
+    }
+
+    public void setYearPublication(int yearPublication) {
+        this.yearPublication = yearPublication;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
+    public static void task() {
+        Author author1 = new Author("Имя1", "Фамилия1");
+        Book book1 = new Book("Книга 1", author1, 2023);
+        Author author2 = new Author("Имя2", "Фамилия2");
+        Book book2 = new Book("Книга 2", author2, 2020);
+        /*System.out.println(book1.getBookTitle() + " " + author1.getName() + " " + author1.getSurname() +
+        " " + book1.getYearPublication());
+        System.out.println(book2.getBookTitle() + " " + author2.getName() + " " + author2.getSurname() +
+                " " + book2.getYearPublication()); */
+        book1.setYearPublication(2022);
+//       System.out.println("book1.getYearPublication() = " + book1.getYearPublication());
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task();
+
     }
 }
